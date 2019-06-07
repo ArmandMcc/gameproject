c17ddfa41c6bbbefe3eae92277b9f3a05b6b18df
ArmandMcc <51391037+ArmandMcc@users.noreply.github.com>
2019-06-06 20:54:21 -0500

    add

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlitzBall.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
